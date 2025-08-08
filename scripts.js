eb8ecb64562da831fe81eff630ee4ac0e8e4d642
alexsteixeira00 <alexs.juci@gmail.com>
2025-08-08 04:22:12 -0300

    projeto cafeteria Dev

---
+++ scripts.js
@@ -0,0 +1,18 @@
+// LÓGICA DE PROGRAMAÇÃO
+// [x]Saber quando o usuário clicou no botão
+// [x] Mudar o posiciinamento do Modal
+// [x] Fazer a máscara ficar visível
+
+const modal = document.querySelector('.modal');
+const mascara = document.querySelector('.mascara-modal');
+
+
+function mostrarModal() {
+    modal.style.left = '50%';
+    mascara.style.visibility = 'visible';
+}
+
+function esconderModal() {
+    modal.style.left = '-19%';
+    mascara.style.visibility = 'hidden';
+}

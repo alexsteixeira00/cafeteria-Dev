90cdb2f140f9a9da05faab86eae314f61c66ab88
alexsteixeira00 <alexs.juci@gmail.com>
2025-08-09 04:07:43 -0300

    alteração no meu código

--- scripts.js
+++ scripts.js
@@ -9,12 +9,10 @@ const mascara = document.querySelector('.mascara-modal');
 
 function mostrarModal() {
     modal.style.left = '50%';
-    modal.style.transform = 'translate(-50%, -50%)';
     mascara.style.visibility = 'visible';
 }
 
 function esconderModal() {
     modal.style.left = '-19%';
-    modal.style.transform = 'translate(-200%, -50%)';
     mascara.style.visibility = 'hidden';
 }
